f4812760d3a9a6f99ed3dd98af69eae6dccc1953
Andrey <andryuha.gonchar@mail.ru>
2024-03-15 16:15:23 +0500

    task1done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void GetListNatNums(int n, int m)
+{
+    if (m == n )
+    {
+        Console.Write($"{m} ") ;
+        return; 
+    }
+    Console.Write($"{m} ");
+    GetListNatNums(n,m + 1);
+}
+
+Console.Write("Введите натуральное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+GetListNatNums(n,m);
