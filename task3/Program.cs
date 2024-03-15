e18fa15f4fc194c754f7ad3aa60ef9237961d8b9
Andrey <andryuha.gonchar@mail.ru>
2024-03-15 17:16:29 +0500

    task3done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] mas = { 1, 2, 5, 0, 10, 34 };
+
+void PrintMasBack(int[] mas, int i = 0)
+{
+    if (i > mas.Length - 1)
+    {
+        return;
+    }
+    PrintMasBack(mas, i + 1);
+    Console.Write($"{mas[i]} ");
+}
+PrintMasBack(mas);
+
+
+
+
+
+
+
+
